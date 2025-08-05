02e93f34e4fbcaf73192624b02c10617232040d1
alireza parvaresh <parvvaresh@gmail.com>
2025-08-05 12:11:32 +0330

    add some codes

---
+++ 02_exported_names/main.go
@@ -0,0 +1,10 @@
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	fmt.Println(math.Pi) // if pi get error In Go, a name is exported if it begins with a capital letter. For example, Pizza is an exported name, as is Pi, which is exported from the math package.
+}
